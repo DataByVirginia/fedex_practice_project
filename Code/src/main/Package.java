100960a2a9b22cdba7c84a823bfd307d3185e380
Virginia Malone <virginiaamalone@gmail.com>
2025-01-29 03:51:38 -0600

    Update Package.java

--- Code/src/main/Package.java
+++ Code/src/main/Package.java
@@ -1,45 +1,44 @@
 package main;
 
+/*
+ I don't want these main attributes to be accessible by any Joe-shmo.  
+ Instead of letting anyone change them directly (which could break stuff),  
+ I'll keep them private and use encapsulation (getters/setters) to control access.  
+ That way, I can validate data, enforce rules, and keep everything running smoothly without constant cleanup.  
+*/
+
 public class Package { // my object is package
-	// I don't want these main attributes to be accessible by any Joe-shmo
-	// I don't want to have to clean-up behind, maintain, or otherwise deal with all
-	// the Joe's shmoie behavior
-	// So it will be private and 'they' (alter-ego Virginia and others) can use
-	// encapsulation to access
+
 	private String id; // unique id
 	private double weight; // carriers use decimals for weight, so I will too
 	private String destination; // the point b -- delivery destination
 
 	// initialize package with a constructor
-	// this is where we use encapsulation to take the private attributes and let
-	// others play w/and change them
+	//This is where we use encapsulation to take the private attributes and let others play w/and change them
 	public Package(String id, double weight, String destination) {
 		this.id = id;
 		this.weight = weight;
-		this.destination = destination; // 'this' is the object we're in -- faster
-		// like renaming a long database/table path: AAD.dbo.t_tran_log AS tl in SQL
-
-		// we've made a container for the info we need to hold about an object
-		// but who's going to retrieve it?
-		// Answer: Getters. These homies are going to act as bouncers:
-		// checking for the appropriate requirements to let them access the fields and
-		// 'get' info
+		this.destination = destination; // 'this' is the object we're in.  
+						// Similar to aliasing in SQL (AAD.dbo.t_tran_log AS tl) to shorten references  
+						// and ensure we pull from the right place when multiple sources overlap.
 	}
 
+// Getters let us safely access private attributes without exposing them directly (similar to "read-only")
+	
 	public String getId() {
-		return id;
+		return id; // returning the unique package ID
 	}
 
 	public double getWeight() {
-		return weight;
+		return weight; // returns the current weight
 	}
 
 	public String getDestination() {
-		return destination;
+		return destination; // returns the stored delivery destination
 	}
 
-	// setters are the ones that take what was 'gotten' and allow it to be changed:
-	// WITH RULES
+// Setters are the sheriffs of this here town! They control what gets updated, making sure nothing shady sneaks past.
+
 	public void setWeight(double weight) {
 		if (weight > 0) {
 			this.weight = weight;
@@ -48,10 +47,12 @@ public class Package { // my object is package
 		}
 	}
 
-	// Encapsulate input validation for weight in this helper method
+// This method validates user input before updating weight. 
+// If the input isn't a valid number, it's tossed out, and the main re-prompts
+
 	public void validateAndSetWeight(String input) {
 		try {
-			double parsedWeight = Double.parseDouble(input); // Attempt to parse the input
+			double parsedWeight = Double.parseDouble(input); // Parse from String to number
 			setWeight(parsedWeight); // Use the setter to validate and set the weight
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("Invalid input! Please enter a numeric value for weight:");
@@ -59,12 +60,10 @@ public class Package { // my object is package
 	}
 
 	public void setDestination(String destination) {
-		this.destination = destination; // Update the destination -- no need to over complicate things for this dip back
-										// in
+		this.destination = destination; // No sheriffs needed ATM. Can update PRN.
 	}
 
-	// This method will output a quick summary of the package -- inputs to be given
-	// in MainClass
+	// This method will output a quick summary of the package -- inputs to be given in MainClass
 	public void printDetails() {
 		System.out.println("Package ID: " + id);
 		System.out.println("Weight: " + weight + " lbs");
